8f5b286a4648e0c1ea21b480d6eea45fb1ddece6
github username <github email address>
2019-09-19 18:02:21 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+    @@all = []
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name 
+        save 
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.print_all 
+        @@all.each do |name|
+            puts name.name
+        end   #need to transform <Dog:0x00007fa7118391a8 @name="Pluto"> to "Pluto"
+    end
+    
+    def save
+        @@all << self
+    end
+    
+    def self.clear_all
+        @@all.clear
+    end
+
+end
